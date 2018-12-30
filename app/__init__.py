f55df364bbb96f02abfb7e96ddd9dff6ad3eb0c6
Martin Siddons <gny17hvu@uea.ac.uk>
2018-12-30 17:50:18 +0000

    Uploading to Github apparently fixed the config issue?! No idea.

--- app/__init__.py
+++ app/__init__.py
@@ -12,15 +12,14 @@ def create_app(config_name):
     app = Flask(__name__)
     app.config.from_object(config['development'])
 
-    from .main import main as main_blueprint
-    app.register_blueprint(main_blueprint)
-
     config['development'].init_app(app)
 
     bootstrap.init_app(app)
     moment.init_app(app)
     db.init_app(app)
 
+    from .main import main as main_blueprint
+    app.register_blueprint(main_blueprint)
 
     # attach routes and custom error pages here
 
