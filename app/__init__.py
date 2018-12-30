6312dffa05433a9c4a6fb700bccbe3f9ab910867
Martin Siddons <gny17hvu@uea.ac.uk>
2018-12-30 23:42:03 +0000

    Issue somehow solved by restart.

--- app/__init__.py
+++ app/__init__.py
@@ -11,9 +11,8 @@ db = SQLAlchemy()
 def create_app(config_name):
     app = Flask(__name__)
     app.config.from_object(config['development'])
-
     config['development'].init_app(app)
-
+    
     bootstrap.init_app(app)
     moment.init_app(app)
     db.init_app(app)
